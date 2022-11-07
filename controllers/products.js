2cc89510bfec9d22b5d0d408a23e4a509443e430
AhmedHaggag720 <AhmedHaggag720@gmail.com>
2022-11-07 18:49:00 +0200

    last

--- controllers/products.js
+++ controllers/products.js
@@ -1,8 +1,62 @@
+const Product = require('../models/product')
+
 exports.getAllProductsStatic = async (req, res, next) => {
-    //throw new Error('testing')
-    res.status(200).json({ msg: 'products testing route' })
+    const search = 'ab';
+    const products = await Product.find({
+        name: { $regex: search, $options: 'i' }
+    });
+
+    res.status(200).json({ products: products, nbHits: products.length })
 }
 
 exports.getAllProducts = async (req, res, next) => {
-    res.status(200).json({ msg: 'products route' })
+    const { featured, company, name, sort, fields, numericFilters } = req.query;
+    const queryObject = {}
+
+    if (featured) {
+        queryObject.featured = featured === 'true' ? true : false;
+    }
+    if (company) {
+        queryObject.company = company;
+    }
+    if (name) {
+        queryObject.name = { $regex: name, $options: 'i' };
+    }
+    if (numericFilters) {
+        const operatorMap = {
+            '>': '$gt',
+            '>=': '$gte',
+            '=': '$eq',
+            '<': '$;t',
+            '<=': '$lte',
+        }
+        const regEx = /\b(<|>|<=|>=|=)\b/g;
+        let filters = numericFilters.replace(regEx,(match)=> `${operatorMap[march]}-`)
+    }
+
+    console.log(queryObject)
+    let result = Product.find(queryObject);
+    //sort
+    if (sort) {
+        const sortList = sort.split(',').join(' ');
+        result = result.sort(sortList)
+        //console.log(result)
+    }
+    else {
+        result = result.sort('createdAt')
+    }
+
+    //fields
+    if (fields) {
+        const fieldsList = fields.split(',').join(' ');
+        result = result.select(fieldsList)
+    }
+
+    const page = Number(req.query.page) || 1;
+    const limit = Number(req.query.limit) || 10;
+
+    const skip = (page - 1) * limit;
+    result = result.skip(skip).limit(limit);
+    const products = await result
+    res.status(200).json({ products: products, nbHits: products.length })
 }
